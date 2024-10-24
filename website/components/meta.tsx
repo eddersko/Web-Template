821048a838f4de2bd01e25ed904f056aa46f11f9
Sam Poder <39828164+sampoder@users.noreply.github.com>
2024-10-23 23:13:57 -0700

    Update meta.tsx

--- website/components/meta.tsx
+++ website/components/meta.tsx
@@ -3,7 +3,7 @@ import Head from "next/head";
 export default function Meta({ title }: { title?: string }) {
   return (
     <Head>
-      <title>{title || "Xaitsnoo Community Dictionary Project"}</title>
+      <title>{`${title ? `${title} | ` : ""}Xaitsnoo Community Dictionary Project`}</title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
     </Head>
   );
